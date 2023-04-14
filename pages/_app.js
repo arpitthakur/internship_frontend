8863c932c367db92189ff048156d70b1d6c62d57
mohitdhimanskt <mohit.dhiman.sarkaghat@gmail.com>
2023-04-14 18:17:14 +0530

    Changes on Components

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,44 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
 import Head from "next/head";
 import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   return (
-  <>
+    <>
+      <Head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+          rel="stylesheet"
+        />
+        <link
+          rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+          crossorigin="anonymous"
+        />
 
-  <Head>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossOrigin="anonymous"/>
-  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
-    </Head>
-  <Component {...pageProps} />
-  <Script
-id="bootstrap-cdn"
-src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" />
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"/>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-  </>
-)}
+        <link
+          rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+          crossorigin="anonymous"
+        />
+      </Head>
+      <Component {...pageProps} />
+      <Script
+        id="bootstrap-cdn"
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+      />
+      <link
+        rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"
+      />
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    </>
+  );
+}
---
+++ pages/login.js
@@ -0,0 +1,114 @@
+import Link from "next/link";
+import React from "react";
+import Header from "../components/Header"
+const login = () => {
+  return (
+    
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4"></div>
+        <div class="col-md-5">
+          <div class="card cardbox">
+            <div class="card-header">Login</div>
+            <div class="card-body">
+              <div class="social-buttons">
+                <a href="#" class="btn btn-md btn-block btn-fb">
+                  <i class="fa fa-facebook"></i> Facebook
+                </a>
+                <a href="#" class="btn btn-md btn-block btn-tw">
+                  <i class="fa fa-twitter"></i> Twitter
+                </a>
+              </div>
+
+              <div class="login-or">
+                <hr class="hr-or"></hr>
+                <span class="span-or">or</span>
+              </div>
+
+              <div class="form-group">
+                <form
+                  id="login-nav"
+                  method="post"
+                  role="form"
+                  class="form"
+                  accept-charset="UTF-8"
+                >
+                  <div class="form-group">
+                    <label class="sr-only">Username</label>
+                    <input
+                      type="text"
+                      id="reg_username"
+                      name="user_name"
+                      class="form-control"
+                      placeholder="Username"
+                      required
+                    />
+                  </div>
+
+                  <div class="form-group">
+                    <label class="sr-only">Password</label>
+
+                    <div class="input-group">
+                      <input
+                        type="password"
+                        id="reg_userpassword"
+                        name="user_password"
+                        class="form-control"
+                        data-placement="bottom"
+                        data-toggle="popover"
+                        data-container="body"
+                        data-html="true"
+                        placeholder="Password"
+                        required
+                      />
+                    </div>
+
+                    <div class="help-block text-right">
+                      <small>
+                        <a href="#">Forgot Password</a>
+                      </small>
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <button
+                      id="reg_submit"
+                      name="submit"
+                      value="1"
+                      class="btn btn-block btn-primary"
+                    >
+                      Login
+                    </button>
+                  </div>
+
+                  <div class="form-check">
+                    <input
+                      type="checkbox"
+                      id="reg_remember"
+                      name="user_remember"
+                      class="form-check-input"
+                    />
+                    <label class="form-check-label">Remember me</label>
+                  </div>
+                </form>
+              </div>
+
+              <div class="login-or">
+                <hr class="hr-or"></hr>
+              </div>
+
+              <div class="bottom text-center">
+                Aren't you user?{" "}
+                <a href="#">
+                  <b>Register</b>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default login;
