6e9d9adf6f637d7680fe29a6ec485a7d2e3d07b5
arpit thakur <arpitthakut722@gmail.com>
2023-04-20 16:16:04 +0530

    student_dashboard

---
+++ pages/student_dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Student_dashboard from "@/components/Student_dashboard";
+
+function student_dashboard() {
+  return (
+    <>
+      <Student_dashboard />
+    </>
+  );
+}
+
+export default student_dashboard;
